54a19d2001f4e570290f9a789558e5b05e91c626
Bernardo Orsi <119913684+beorsi@users.noreply.github.com>
2024-08-06 13:31:50 +0000

    Update blur JS

--- a.js
+++ a.js
@@ -1,4 +1,11 @@
-function bluri(){
+function corBlur(){
     let corpo = document.body
     corpo.classList.toggle("corBlur")
 }
+
+const btnPesquisa = document.getElementById("pesquisa")
+const barraPesquisa = document.getElementById("Barra")
+
+btnPesquisa.addEventListener("click", () => {
+    barraPesquisa.showModal();
+})
